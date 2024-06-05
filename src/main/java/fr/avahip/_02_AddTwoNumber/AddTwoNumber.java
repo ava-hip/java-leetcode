dc8370eb86f89e0b6fc83e78f7be49e69e7b8838
Hippolyte Gautheron <hgautheron@dawan.fr>
2024-06-05 11:34:18 +0200

    add: 2nd leet code

---
+++ src/main/java/fr/avahip/_02_AddTwoNumber/AddTwoNumber.java
@@ -0,0 +1,13 @@
+package fr.avahip._02_AddTwoNumber;
+
+public class AddTwoNumber {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        // LeetCode - https://leetcode.com/problems/add-two-numbers/description/
+        System.out.println("https://leetcode.com/problems/add-two-numbers/description/");
+    }
+}
